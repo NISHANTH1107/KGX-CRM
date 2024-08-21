0b5c243419d401398c4df015f7e05351c9b991bf
Roshan <nagaroshan_22aib06@kgkite.ac.in>
2024-08-21 14:04:31 +0530

    modified templates

--- kgx_app/management/commands/run_scheduled_job.py
+++ kgx_app/management/commands/run_scheduled_job.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from datetime import timedelta
-import pytz
+import pytz # type: ignore
 import os
 import schedule
 import time
