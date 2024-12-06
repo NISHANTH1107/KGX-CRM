5617a7617d529e320a6aaf488b59820c45c2b743
NISHANTH1107 <nishanthmn07@gmail.com>
2024-12-07 00:34:34 +0100

    added domain

--- kgx_project/settings.py
+++ kgx_project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-ww7!nr19%jr9lr@zh&3vy%d4a&p#eubq!t+l920^7(%-#i!(a%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nishanth11.pythonanywhere.com']
 
 
 # Application definition
